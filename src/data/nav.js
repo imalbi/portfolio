52d7f007efebf72aac21bd94ec9b9b100ea12479
imalbi <alberto.cerqua@icloud.com>
2025-06-26 16:36:31 +0200

    feat: navBar

---
+++ src/data/nav.js
@@ -0,0 +1,8 @@
+export let items=[
+    {id:1,text:"Home",href:"#home"},
+    {id:2,text:"About",href:"#about"},
+    {id:3,text:"Goals",href:"#goals"},
+    {id:4,text:"Projects",href:"#projects"},
+    {id:5,text:"Resume",href:"#resume"},
+    {id:6,text:"Contacts",href:"#contacts"}
+]
