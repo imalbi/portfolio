f67a653793ae28ae8fdebadf37494cffc12e8801
imalbi <alberto.cerqua@icloud.com>
2025-06-28 08:29:08 +0200

    feat: mobile nav

---
+++ src/components/MobileNav.svelte
@@ -0,0 +1,14 @@
+<script>
+	import NavButton from './navButton.svelte';
+	import { items } from '../data/nav';
+
+	let isVisibleMobile = $state(false);
+</script>
+
+<nav class="h-fit border-t-1 border-slate-600 bg-slate-950 md:hidden">
+	<ul class="flex w-full flex-col">
+		{#each items as item (item.id)}
+			<NavButton text={item.text} href={item.href}></NavButton>
+		{/each}
+	</ul>
+</nav>
